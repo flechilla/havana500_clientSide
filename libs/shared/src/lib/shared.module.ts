97d50bed8983a74c57e82e8ffd5fe6be0d0a2f6d
NachoVazquez <nachovc1410@gmail.com>
2018-08-30 17:55:12 -0400

    Set components, modules, pipes ,etc to Shared Lib

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from './modules/material.module';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { KeysPipe } from './pipes/keys.pipe';
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, MaterialModule],
+  declarations: [ConfirmDialogComponent, KeysPipe],
+  exports: [MaterialModule, ConfirmDialogComponent, KeysPipe]
 })
 export class SharedModule {}
