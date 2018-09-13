0ce06cb27cf53b6db087e0cac60ff30fa6f439e9
NachoVazquez <nachovc1410@gmail.com>
2018-09-13 15:23:34 -0400

    Fix environment selection

--- apps/havana500-admin/src/app/core/services/http/article.service.ts
+++ apps/havana500-admin/src/app/core/services/http/article.service.ts
@@ -1,9 +1,8 @@
 import { Article } from './../../models/article.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
 import { BaseCrudService } from '../../../shared/services/base-crud.service';
-import { environment } from '../../../../environments/environment.prod';
 
 @Injectable()
 export class ArticleService extends BaseCrudService<Article> {
