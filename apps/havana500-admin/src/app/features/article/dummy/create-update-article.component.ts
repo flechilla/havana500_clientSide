2f08464cec77721ba0f0101d579d3f7fedacaa37
NachoVazquez <nachovc1410@gmail.com>
2018-09-13 15:24:56 -0400

    Add sections selection to ArticlesCreateUpdateComponent
    * Add other style fixes

--- apps/havana500-admin/src/app/features/article/dummy/create-update-article.component.ts
+++ apps/havana500-admin/src/app/features/article/dummy/create-update-article.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Inject,
+  ViewEncapsulation,
+  AfterViewInit
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Article } from '../../../core/models/article.model';
@@ -6,6 +12,8 @@ import { ArticleService } from '../../../core/services/http/article.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { antAnimations } from '../../../shared/utils/animations';
+import { Section } from '../../../core/models/section.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ant-create-update-article',
@@ -18,21 +26,31 @@ export class CreateUpdateArticleComponent implements OnInit {
   protected form: FormGroup;
   protected onEdit = false;
 
+  protected sections: Section[];
+
   constructor(
     protected fb: FormBuilder,
     protected dialogRef: MatDialogRef<CreateUpdateArticleComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Article
+    @Inject(MAT_DIALOG_DATA)
+    public data: { article: Article; sections: Section[] }
   ) {}
 
   ngOnInit() {
-    if (this.data) {
+    this.loadForm();
+
+    if (!this.data) {
+      console.error(
+        'Data passed to dialog must not be empty and must have a list of Sections'
+      );
+    }
+    this.sections = this.data.sections;
+
+    if (this.data.article) {
       this.onEdit = true;
     }
 
-    this.loadForm();
-
     if (this.onEdit) {
-      this.form.get('article').patchValue(this.data);
+      this.form.get('article').patchValue(this.data.article);
     }
   }
 
