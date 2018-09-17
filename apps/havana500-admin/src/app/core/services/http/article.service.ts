0f75cbbfc97a065f4acaf56605ef8be8d2df3a53
NachoVazquez <nachovc1410@gmail.com>
2018-09-17 13:20:16 -0400

    Add getWithTags method to ArticleService

--- apps/havana500-admin/src/app/core/services/http/article.service.ts
+++ apps/havana500-admin/src/app/core/services/http/article.service.ts
@@ -3,10 +3,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { BaseCrudService } from '../../../shared/services/base-crud.service';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ArticleExtended } from '../../models/article-extended';
 
 @Injectable()
 export class ArticleService extends BaseCrudService<Article> {
   constructor(private httpClient: HttpClient) {
     super(environment.apiUrl + 'articles', httpClient);
+  }
+
+  /**
+   * getWithTags
+   */
+  public getWithTags(id: number): Observable<ArticleExtended> {
+    return this.http
+      .get<ArticleExtended>(this.url + '/GetArticleWithTags?articleId=' + id)
+      .pipe(catchError(this.handleError));
   }
 }
