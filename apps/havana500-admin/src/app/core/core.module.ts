0be00644f6e282c173809f24541aca7716c14e89
NachoVazquez <nachovc1410@gmail.com>
2018-09-21 17:44:40 -0400

    Add CoreModule protection to be imported just once

--- apps/havana500-admin/src/app/core/core.module.ts
+++ apps/havana500-admin/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { OAuthWrapperService } from './services/oauth-wrapper.service';
 import { AuthenticatedGuard } from './route_guards/authenticated.guard';
@@ -28,4 +28,15 @@ import { SectionService } from './services/http/section.service';
     SectionService
   ]
 })
-export class CoreModule {}
+export class CoreModule {
+  /* make sure CoreModule is imported only by one NgModule the AppModule */
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
