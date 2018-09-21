e75b65ba595edda25b0f32447f34235a881062cd
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-21 01:28:42 -0400

    Override the method to get the items with pagination

--- apps/havana500-admin/src/app/core/services/http/article-comments-info.service.ts
+++ apps/havana500-admin/src/app/core/services/http/article-comments-info.service.ts
@@ -5,6 +5,7 @@ import { BaseCrudService } from '../../../shared/services/base-crud.service';
 import { ArticleCommentsInfo } from '../../models/articleCommentsInfo';
 import { Observable } from 'rxjs';
 import { CommentModule } from '../../../features/comment/comment.module';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ArticleCommentsInfoService extends BaseCrudService<ArticleCommentsInfo> {
@@ -21,5 +22,34 @@ export class ArticleCommentsInfoService extends BaseCrudService<ArticleCommentsI
     const options = daysAgo ?
     { params: new HttpParams().set('daysAgo', daysAgo.toString()) } : {}
     return this.httpClient.get<ArticleCommentsInfo[]>(`${this.url}/GetArticlesWithNewCommentsInfo`, options)
+}
+
+public getWithPagAndSort(
+  pageNumber: number,
+  pageSize: number,
+  columnToSort: string,
+  sortDir: string
+): Observable<ArticleCommentsInfo[]> {
+  const daysAgo = 50000;
+  // const options =  { params: new HttpParams()
+  //         .set('daysAgo', '500')
+  //         .set('pageNumber', pageNumber.toString())
+  //         .set() } 
+  console.log('exec the overrided method');
+  return this.http
+    .get<ArticleCommentsInfo[]>(
+      this.url +
+        '/GetArticlesWithNewCommentsInfo?pageNumber=' +
+        pageNumber +
+        '&pageSize=' +
+        pageSize +
+        '&columnNameForSorting=' +
+        columnToSort +
+        '&sortingType=' +
+        sortDir+
+        '&daysAgo='+daysAgo
+
+    )
+    .pipe(catchError(this.handleError));
 }
 }
