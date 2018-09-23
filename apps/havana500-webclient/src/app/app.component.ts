d5324e6463def26a45711387f972d9fb09be6c3e
NachoVazquez <nachovc1410@gmail.com>
2018-09-23 15:27:25 -0400

    Add security protections

--- apps/havana500-webclient/src/app/app.component.ts
+++ apps/havana500-webclient/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AccountSandbox } from './core/sandboxes/account-sandbox';
+import { isNullOrUndefined } from 'util';
+import { OAuthService, EventType } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'ant-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'havana500-webclient';
+  title = 'havana500-admin';
+
+  constructor(
+    private accountSandbox: AccountSandbox,
+    private oauthService: OAuthService
+  ) {
+    // Set to the store the logged user
+    this.accountSandbox.getUserClaims().subscribe(claims => {
+      if (
+        isNullOrUndefined(claims) &&
+        !isNullOrUndefined(this.accountSandbox.getUserClaimsFromLocalStorage())
+      ) {
+        this.accountSandbox.setUserClaimsState();
+      }
+    });
+
+    this.oauthService.events.subscribe(event => {
+      if (event.type === 'token_expires') {
+        this.accountSandbox.refreshToken();
+      }
+    });
+  }
 }
