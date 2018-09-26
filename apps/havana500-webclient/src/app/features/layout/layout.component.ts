8d97d7031d5544a6371eeeb3179f5bdfe2267238
NachoVazquez <nachovc1410@gmail.com>
2018-09-26 16:07:39 -0400

    Set base layout for Havana500-web
    * `TopNavBar`
    * `Footer`
    * `Content`

---
+++ apps/havana500-webclient/src/app/features/layout/layout.component.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+
+@Component({
+  selector: 'ant-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LayoutComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnDestroy(): void {}
+
+  ngOnInit() {}
+}
---
+++ apps/havana500-webclient/src/app/features/layout/main-menu/toolbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'ant-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class AntToolbarComponent implements OnInit {
+  languages: any;
+  selectedLanguage: any;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.languages = [
+      { id: 'en', title: 'English', flag: 'us' },
+      { id: 'fr', title: 'French', flag: 'fr' }
+    ];
+
+    this.selectedLanguage = this.languages[0];
+  }
+
+  setLanguage(lang) {
+    // Set the selected language for toolbar
+    this.selectedLanguage = lang;
+
+    // Use the selected language for translations
+    // this.translate.use(lang.id);
+  }
+}
