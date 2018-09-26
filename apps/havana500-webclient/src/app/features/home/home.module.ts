f3881a0cfd452e7e9a62bca21c46c1069b582a2c
NachoVazquez <nachovc1410@gmail.com>
2018-09-26 16:08:03 -0400

    Create empty home module

---
+++ apps/havana500-webclient/src/app/features/home/home.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './smart/main/home.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [HomeComponent],
+  providers: []
+})
+export class HomeModule {}
