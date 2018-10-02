3e45d6efdfc9359e983459ce5dc5a4d407774e78
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-02 09:48:38 -0400

    Imports the ArticleService
    The service of article should be in the shared folder, because
    it has to be used from the admin and the client.
    
    There is an error with the ArticleService import, it says that this
    class doesn't have a default export :(

--- libs/shared/src/lib/components/article/article.component.ts
+++ libs/shared/src/lib/components/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ArticleService from '../../../../../../apps/havana500-webclient/src/app/core/services/http/article.service';
 
 @Component({
   selector: 'ant-article',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private articleService: ArticleService) { 
+  }
 
   ngOnInit() {
   }
