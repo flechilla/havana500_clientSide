04c7c1885ee0456b5c87373ef3d271c15cb92f24
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-02 21:17:45 -0400

    Finish the basic layout of the Article view, without any style

--- libs/shared/src/lib/components/article/article.component.ts
+++ libs/shared/src/lib/components/article/article.component.ts
@@ -1,9 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Location } from '@angular/common';
-import {ArticleService} from '../../services/http/article.service';
-import {ArticleExtended} from '../../models/article-extended';
-import { switchMap } from 'rxjs/operators';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  ActivatedRoute,
+  ParamMap
+} from '@angular/router';
+import {
+  Location
+} from '@angular/common';
+import {
+  ArticleService
+} from '../../services/http/article.service';
+import {
+  ArticleExtended
+} from '../../models/article-extended';
+import {
+  switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'ant-article',
@@ -13,9 +27,8 @@ import { switchMap } from 'rxjs/operators';
 export class ArticleComponent implements OnInit {
   private article: ArticleExtended;
   constructor(private articleService: ArticleService,
-  private route: ActivatedRoute, 
-private location: Location) { 
-  }
+    private route: ActivatedRoute,
+    private location: Location) {}
 
   ngOnInit() {
     // this.route.paramMap.pipe(
@@ -27,11 +40,15 @@ private location: Location) {
 
   }
 
-  getArticle(): void{
-  const articleId = +this.route.snapshot.paramMap.get('id');
+  getArticle(): void {
+    const articleId = +this.route.snapshot.paramMap.get('id');
 
     this.articleService.getWithTags(articleId).
-    subscribe(article=>this.article = article);
+    subscribe(article => this.article = article);
+  }
+
+  onSelectTag(tagId: number) : void{
+    console.log('this method is hit after the user selelects a tag. the tag Id is: '+tagId);
   }
 
 }
