b64f1e6eef7d139877d744b2fdb9338b83ba8c05
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-03 20:48:37 -0400

    Minor changes.
    NOw all is working as expected

--- apps/havana500-webclient/src/app/app-routes.ts
+++ apps/havana500-webclient/src/app/app-routes.ts
@@ -1,5 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
-import { ArticleComponent } from 'libs/shared/src/lib/components/article/article.component';
+import { ArticleComponent } from '@hav500workspace/shared';
 import { NgModule } from '@angular/core';
 
 export const appRoutes: Routes = [
