cf306df2f81d68d56a9bbd863878114f323b62bb
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-06 23:48:33 -0400

    Adds some comments

--- libs/shared/src/lib/components/article/article.component.ts
+++ libs/shared/src/lib/components/article/article.component.ts
@@ -7,6 +7,7 @@ import {  switchMap} from 'rxjs/operators';
 import { Article } from '../../models';
 import {Comment} from '../../models';
 import { CommentService } from '../../services';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ant-article',
@@ -16,6 +17,7 @@ import { CommentService } from '../../services';
 export class ArticleComponent implements OnInit {
   private article: ArticleExtended;
   private relatedArticles: Article[];
+  private asyncArticle: Observable<Article>;
 
   constructor(private articleService: ArticleService,
     private route: ActivatedRoute, 
@@ -32,7 +34,13 @@ export class ArticleComponent implements OnInit {
     this.getArticle();
     this.getRelatedArticles();
   }
-
+  /**
+   *  Get the article with the given articleId that is in the route.
+   * 
+   *  This implementation can be improved by using switchMap. This should be used
+   *  when the component is gonna be re-used, like in this case where the user can 
+   *  change to other article.
+   */
   getArticle(): void {
     const articleId = +this.route.snapshot.paramMap.get('id');
 
@@ -44,7 +52,12 @@ export class ArticleComponent implements OnInit {
     console.log('this method is hit after the user selelects a tag. the tag Id is: '+tagId);
   }
 
-  getRelatedArticles(){
+  /**
+   *  Get the related articles of the current article. 
+   * 
+   * Can be improved with the swithMap
+   */
+  getRelatedArticles() : void{
     const articleId = +this.route.snapshot.paramMap.get('id');
     this.articleService.getRelatedArticles(articleId).
     subscribe(articles => this.relatedArticles = articles);
