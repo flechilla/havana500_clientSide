361e997aa4a3db2972d51c66b61486ceeeead439
NachoVazquez <nachovc1410@gmail.com>
2018-10-10 18:16:45 -0400

    Fixing routing of articles in the same component

--- libs/shared/src/lib/components/article/article.component.ts
+++ libs/shared/src/lib/components/article/article.component.ts
@@ -1,36 +1,12 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  ActivatedRoute,
-  ParamMap,
-  Router
-} from '@angular/router';
-import {
-  Location
-} from '@angular/common';
-import {
-  ArticleService
-} from '../../services/http/article.service';
-import {
-  ArticleExtended
-} from '../../models/article-extended';
-import {
-  switchMap
-} from 'rxjs/operators';
-import {
-  Article
-} from '../../models';
-import {
-  Comment
-} from '../../models';
-import {
-  CommentService
-} from '../../services';
-import {
-  Observable
-} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { ArticleService } from '../../services/http/article.service';
+import { ArticleExtended } from '../../models/article-extended';
+import { switchMap } from 'rxjs/operators';
+import { Article } from '../../models';
+import { CommentService } from '../../services';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ant-article',
@@ -38,51 +14,43 @@ import {
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
-  relatedArticles$: Observable < Article[] > ;
-  article$: Observable < ArticleExtended > ;
+  relatedArticles$: Observable<Article[]>;
+  article$: Observable<ArticleExtended>;
 
-  constructor(private articleService: ArticleService,
+  constructor(
+    private articleService: ArticleService,
     private route: ActivatedRoute,
     private location: Location,
     private commentService: CommentService,
-    private router: Router) {}
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.getArticle();
-    this.getRelatedArticles(); 
-
-    //TODO: Update the comment. For this have to invoke the method inthe comment-home component.
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id: number = +params.get('id');
+      this.getArticle(id);
+      this.getRelatedArticles(id);
+    });
   }
+
   /**
    *  Get the article with the given articleId that is in the route.
-   */ 
-  getArticle(): void {
-    this.article$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => {
-        return this.articleService.getWithTags(+params.get('id'));
-        }
-      )
-    );
+   */
+  getArticle(id: number): void {
+    this.article$ = this.articleService.getWithTags(id);
   }
 
   onSelectTag(tagId: number): void {
-    console.log('this method is hit after the user selelects a tag. the tag Id is: ' + tagId);
-  }
-
-  /**
-   *  Get the related articles of the current article. 
-   */
-  getRelatedArticles(): void {
-    this.relatedArticles$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => {
-        return this.articleService.getRelatedArticles(+params.get('id'));
-        }
-      )
+    console.log(
+      'this method is hit after the user selelects a tag. the tag Id is: ' +
+        tagId
     );
   }
 
-
-
-
-
+  /**
+   *  Get the related articles of the current article.
+   */
+  getRelatedArticles(id: number): void {
+    this.relatedArticles$ = this.articleService.getRelatedArticles(id);
+  }
 }
