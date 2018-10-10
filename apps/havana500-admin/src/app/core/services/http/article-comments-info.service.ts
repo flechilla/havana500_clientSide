75d0de4cf76ac5487c687a77f3f03eb27d67fded
NachoVazquez <nachovc1410@gmail.com>
2018-10-10 14:19:39 -0400

    Clean up article-comment-info-service

--- apps/havana500-admin/src/app/core/services/http/article-comments-info.service.ts
+++ apps/havana500-admin/src/app/core/services/http/article-comments-info.service.ts
@@ -8,48 +8,52 @@ import { CommentModule } from '../../../features/comment/comment.module';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class ArticleCommentsInfoService extends BaseCrudService<ArticleCommentsInfo> {
+export class ArticleCommentsInfoService extends BaseCrudService<
+  ArticleCommentsInfo
+> {
   constructor(private httpClient: HttpClient) {
     super(environment.apiUrl + 'articles', httpClient);
   }
 
-   /**
+  /**
    * Returns the amount of new comments per each article
    * @param  {number} daysAgo The amount of days since the comments were added
    * @returns Observable A list with the name of the Article, the Id and the amount of new comments
    */
-  getArticlesWithNewCommentsInfo(daysAgo: number): Observable<ArticleCommentsInfo[]>{
-    const options = daysAgo ?
-    { params: new HttpParams().set('daysAgo', daysAgo.toString()) } : {}
-    return this.httpClient.get<ArticleCommentsInfo[]>(`${this.url}/GetArticlesWithNewCommentsInfo`, options)
-}
+  getArticlesWithNewCommentsInfo(
+    daysAgo: number
+  ): Observable<ArticleCommentsInfo[]> {
+    const options = daysAgo
+      ? { params: new HttpParams().set('daysAgo', daysAgo.toString()) }
+      : {};
+    return this.httpClient.get<ArticleCommentsInfo[]>(
+      `${this.url}/GetArticlesWithNewCommentsInfo`,
+      options
+    );
+  }
 
-public getWithPagAndSort(
-  pageNumber: number,
-  pageSize: number,
-  columnToSort: string,
-  sortDir: string
-): Observable<ArticleCommentsInfo[]> {
-  const daysAgo = 50000;
-  // const options =  { params: new HttpParams()
-  //         .set('daysAgo', '500')
-  //         .set('pageNumber', pageNumber.toString())
-  //         .set() } 
-  console.log('exec the overrided method');
-  return this.http
-    .get<ArticleCommentsInfo[]>(
-      this.url +
-        '/GetArticlesWithNewCommentsInfo?pageNumber=' +
-        pageNumber +
-        '&pageSize=' +
-        pageSize +
-        '&columnNameForSorting=' +
-        columnToSort +
-        '&sortingType=' +
-        sortDir+
-        '&daysAgo='+daysAgo
+  public getWithPagAndSort(
+    pageNumber: number,
+    pageSize: number,
+    columnToSort: string,
+    sortDir: string
+  ): Observable<ArticleCommentsInfo[]> {
+    const daysAgo = 50000;
 
-    )
-    .pipe(catchError(this.handleError));
-}
+    return this.http
+      .get<ArticleCommentsInfo[]>(
+        this.url +
+          '/GetArticlesWithNewCommentsInfo?pageNumber=' +
+          pageNumber +
+          '&pageSize=' +
+          pageSize +
+          '&columnNameForSorting=' +
+          columnToSort +
+          '&sortingType=' +
+          sortDir +
+          '&daysAgo=' +
+          daysAgo
+      )
+      .pipe(catchError(this.handleError));
+  }
 }
