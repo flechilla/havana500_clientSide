da1e8265ad996765864e5051a098475ddd81a9af
NachoVazquez <nachovc1410@gmail.com>
2018-10-10 14:22:12 -0400

    Fix location of Paginator and clenup commented and messy code

--- apps/havana500-admin/src/app/features/comment/smart/comment-home.component.ts
+++ apps/havana500-admin/src/app/features/comment/smart/comment-home.component.ts
@@ -1,30 +1,7 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  ViewEncapsulation,
-  Input,
-  AfterViewInit
-} from '@angular/core';
-import {
-  MatPaginator,
-  MatSort,
-  MatTableDataSource,
-  MatInput,
-  MatDialog
-} from '@angular/material';
-import { Observable, BehaviorSubject, from, merge } from 'rxjs';
-import { DataSource } from '@angular/cdk/table';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { antAnimations } from '../../../shared/utils/animations';
-import { Article } from '../../../core/models/article.model';
-import { ArticleService } from '../../../core/services/http/article.service';
-import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs/operators';
-import { BaseDataSource } from '../../../shared/utils/base-data-source';
 import { BaseTableContainerComponent } from '../../../shared/components/base-table-container.component';
-import { ActivatedRouteSnapshot } from '@angular/router';
-import { SectionService } from '../../../core/services/http/section.service';
-import { Section } from '../../../core/models/section.model';
 import { ArticleCommentsInfo } from '../../../core/models/articleCommentsInfo';
 import { CommentService } from '../../../core/services/http/comment.service';
 import { ArticleCommentsInfoService } from '../../../core/services/http/article-comments-info.service';
@@ -36,30 +13,21 @@ import { ArticleCommentsInfoService } from '../../../core/services/http/article-
   animations: [antAnimations]
   // encapsulation: ViewEncapsulation.None
 })
-export class CommentHomeComponent extends BaseTableContainerComponent<ArticleCommentsInfo>
+export class CommentHomeComponent
+  extends BaseTableContainerComponent<ArticleCommentsInfo>
   implements OnInit, AfterViewInit {
-  protected dialogRef: any;
-  protected columnsToDisplay: string[] = ['id', 'title', 'amountOfComments', 'startDateUtc'];  
-
-    private articleCommentsInfos: ArticleCommentsInfo[];
-
-  constructor(
-    private commentService: CommentService,
-    private articleCommentsInfoService: ArticleCommentsInfoService,
-    protected dialog: MatDialog
-  ) {
-    super(['id', 'title', 'amountOfComments', 'startDateUtc'], articleCommentsInfoService)
+  constructor(private articleCommentsInfoService: ArticleCommentsInfoService) {
+    super(
+      ['id', 'title', 'amountOfComments', 'startDateUtc'],
+      articleCommentsInfoService
+    );
   }
 
   ngOnInit() {
     super.ngOnInit();
-    this.articleCommentsInfoService.getWithPagAndSort(1, 30, "id", 'asc')
-      .subscribe(result => {this.articleCommentsInfos = result; console.log(this.articleCommentsInfos);});
   }
 
   ngAfterViewInit(): void {
     super.ngAfterViewInit();
   }
-
- 
 }
