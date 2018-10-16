b059d3cdf5cbcb74d4980086749470a6b611c822
NachoVazquez <nachovc1410@gmail.com>
2018-10-16 16:38:06 -0400

    Rename Second-Level to Default instead of deafult

---
+++ apps/havana500-webclient/src/app/features/second-level/second-level-default/second-level-default.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import {
+  ArticleService,
+  Article,
+  ContentTag,
+  ContentTagService
+} from '@hav500workspace/shared';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'hav-second-level-default',
+  templateUrl: './second-level-default.component.html',
+  styleUrls: ['./second-level-default.component.scss']
+})
+export class SecondLevelDefaultComponent implements OnInit {
+  globalTags$: Observable<ContentTag[]>;
+  articles$: Observable<Article[]>;
+  private amountOfArticles = 50;
+  private currentPage = 0;
+  sectionName: string;
+
+  selectedItems: any[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService,
+    private contentTagService: ContentTagService
+  ) {}
+
+  ngOnInit() {
+    this.globalTags$ = this.contentTagService.getAll();
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.sectionName = params.get('sectionName');
+      this.getArticles();
+    });
+  }
+
+  protected getArticles(tagIds: number[] = []): void {
+    this.articles$ = this.articleService.getArticlesBasicDataBySectionNameAndTagIds(
+      this.sectionName,
+      tagIds,
+      this.currentPage,
+      this.amountOfArticles
+    );
+  }
+
+  public tagSelectFilter(term: string, item: ContentTag): boolean {
+    return item.name.toLowerCase().includes(term.toLowerCase());
+  }
+
+  tagSelectionChanged(selectedTags: any[]) {
+    // console.log(selectedTags);
+    // console.log(this.selectedItems);
+    this.getArticles(this.selectedItems);
+  }
+}
