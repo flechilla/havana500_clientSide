83909121a675c15a2673539cbc09c35a86dca00b
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-17 08:28:25 -0400

    Implements method to intercept the trigger of the menu
    This is to set the top value of the nested menus, because
    it's painting it on the main menu

--- apps/havana500-webclient/src/app/features/layout/main-menu/toolbar.component.ts
+++ apps/havana500-webclient/src/app/features/layout/main-menu/toolbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Output, EventEmitter, InjectionToken } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, InjectionToken, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { SectionService, Section } from '@hav500workspace/shared';
 import {Location} from '@angular/common';
+import { MatMenuTrigger } from '@angular/material';
 
 @Component({
   selector: 'hav-toolbar',
@@ -10,7 +11,8 @@ import {Location} from '@angular/common';
   providers: [SectionService]
 })
 export class AntToolbarComponent implements OnInit {
-  
+  @ViewChild(MatMenuTrigger)
+    private menuTrigger: MatMenuTrigger;
 
   constructor(private router: Router, 
     private sectionService: SectionService,
@@ -59,5 +61,13 @@ export class AntToolbarComponent implements OnInit {
    */
   goToSection(sectionName: string) : void{
     location.assign('/section/'+sectionName);
+  }
+
+  onSelect(event: any) : void{
+    const menu = document.getElementById('entertainment');
+    console.log(JSON.stringify(menu));
+    menu.style.display = '';
+    menu.style.top = '65px';
+
   }
 }
