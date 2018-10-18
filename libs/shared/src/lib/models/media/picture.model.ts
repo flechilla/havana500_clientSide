8316d77d232192ad46acd5c07648daedc05660ca
NachoVazquez <nachovc1410@gmail.com>
2018-10-18 12:04:09 -0400

    Add missing properties to pictureModel and create PictureModelExtended

--- libs/shared/src/lib/models/media/picture.model.ts
+++ libs/shared/src/lib/models/media/picture.model.ts
@@ -66,9 +66,9 @@ export interface Picture extends AuditableAndTrackableEntity<number> {
    */
   fullPath: string;
 
-    /**
+  /**
    *Gets or sets the value that indicate the realtive path
-   *in the server to the Picture. We have to 
+   *in the server to the Picture. We have to
    * prefix it the domain of the server.
    *
    * @type {string}
@@ -85,7 +85,7 @@ export interface Picture extends AuditableAndTrackableEntity<number> {
    */
   pictureExtension: string;
 
-    /**
+  /**
    *Gets or sets the value that contains the url where we should
    * redirect the user after a click on the image.
    *
@@ -93,4 +93,46 @@ export interface Picture extends AuditableAndTrackableEntity<number> {
    * @memberof Picture
    */
   href: string;
+
+  /**
+   * Gets or sets the value that indicates that this
+   * picture is active, which means that can be
+   * displayed for the users.
+   *
+   * @type {boolean}
+   * @memberof Picture
+   */
+  isActive: boolean;
+
+  /**
+   * Gets or sets the importance or weight of a Picture
+   *
+   * @type {number}
+   * @memberof Picture
+   */
+  weight: number;
+
+  /**
+   * Gets or sets the name of the picture
+   *
+   * @type {string}
+   * @memberof Picture
+   */
+  name: string;
+
+  /**
+   * Gets or sets the name of the company owning the picture
+   *
+   * @type {string}
+   * @memberof Picture
+   */
+  companyName: string;
+
+  /**
+   * Gets or sets the language used for the name of the picture
+   *
+   * @type {string}
+   * @memberof Picture
+   */
+  languageCulture: string;
 }
