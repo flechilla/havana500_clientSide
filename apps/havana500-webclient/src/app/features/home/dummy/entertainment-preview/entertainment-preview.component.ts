f3b3c37760e536e520bca12000311a22eb0a9f08
NachoVazquez <nachovc1410@gmail.com>
2018-10-31 16:32:10 -0400

    Move components to its original place

--- apps/havana500-webclient/src/app/features/home/dummy/entertainment-preview/entertainment-preview.component.ts
+++ apps/havana500-webclient/src/app/features/home/dummy/entertainment-preview/entertainment-preview.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article, ArticleService } from '@hav500workspace/shared';
 
 @Component({
   selector: 'hav-entertainment-preview',
-    templateUrl: 'entertainment-preview.component.html'
+  templateUrl: 'entertainment-preview.component.html'
 })
 export class EntertainmentComponent implements OnInit {
-  constructor() {}
+  protected sportArticles: Observable<Article[]>;
+  protected literatureArticles: Observable<Article[]>;
+  protected cultureArticles: Observable<Article[]>;
+  protected cinemaArticles: Observable<Article[]>;
 
-  ngOnInit() {}
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit() {
+    this.cinemaArticles = this.articleService.getArticlesBasicDataBySectionName(
+      'cine',
+      0,
+      8
+    );
+    this.sportArticles = this.articleService.getArticlesBasicDataBySectionName(
+      'deportes',
+      0,
+      8
+    );
+    this.cultureArticles = this.articleService.getArticlesBasicDataBySectionName(
+      'cultura',
+      0,
+      8
+    );
+    this.literatureArticles = this.articleService.getArticlesBasicDataBySectionName(
+      'literatura',
+      0,
+      8
+    );
+  }
 }
