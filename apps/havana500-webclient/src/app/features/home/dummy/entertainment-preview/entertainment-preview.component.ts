0ebe82f7de25f72422ff19e09ba2619e06972528
NachoVazquez <nachovc1410@gmail.com>
2018-10-31 19:20:18 -0400

    Finish first iteration over entertainment section
    * Reduce the number of element to 6 and 3 in each row for desktop
    * Show only picture and title for summary articles
    * Add section title 
    * Add see-more link to bottom of section
    * Redirects

--- apps/havana500-webclient/src/app/features/home/dummy/entertainment-preview/entertainment-preview.component.ts
+++ apps/havana500-webclient/src/app/features/home/dummy/entertainment-preview/entertainment-preview.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Article, ArticleService } from '@hav500workspace/shared';
 
 @Component({
   selector: 'hav-entertainment-preview',
-  templateUrl: 'entertainment-preview.component.html'
+  templateUrl: 'entertainment-preview.component.html',
+  styleUrls: ['entertainment-preview.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EntertainmentComponent implements OnInit {
   protected sportArticles: Observable<Article[]>;
@@ -18,22 +20,22 @@ export class EntertainmentComponent implements OnInit {
     this.cinemaArticles = this.articleService.getArticlesBasicDataBySectionName(
       'cine',
       0,
-      8
+      6
     );
     this.sportArticles = this.articleService.getArticlesBasicDataBySectionName(
       'deportes',
       0,
-      8
+      6
     );
     this.cultureArticles = this.articleService.getArticlesBasicDataBySectionName(
       'cultura',
       0,
-      8
+      6
     );
     this.literatureArticles = this.articleService.getArticlesBasicDataBySectionName(
       'literatura',
       0,
-      8
+      6
     );
   }
 }
