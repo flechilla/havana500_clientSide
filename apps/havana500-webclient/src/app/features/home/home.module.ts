0651d4f81ed48922ebd1fd189848c94d63abb225
NachoVazquez <nachovc1410@gmail.com>
2018-11-01 18:38:14 -0400

    Some style fixes to entertainment-preview component

--- apps/havana500-webclient/src/app/features/home/home.module.ts
+++ apps/havana500-webclient/src/app/features/home/home.module.ts
@@ -1,3 +1,4 @@
+import { OutstandingGeneralArticlesComponent } from './dummy/outstanding-general-articles/outstanding-general-articles.component';
 import { OutstandingGaleryMediasComponent } from './dummy/outstanding-galery-medias/outstanding-galery-medias.component';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './smart/main/home.component';
@@ -5,7 +6,6 @@ import { SharedModule } from '@hav500workspace/shared';
 import { TranslateModule } from '@ngx-translate/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { PrincipalSliderComponent } from './dummy/principal-slider/principal-slider.component';
-import { OutstandingNewsComponent } from './dummy/outstanding-news/outstanding-news.component';
 import { OutstandingExperiencesComponent } from './dummy/outstanding-experiences/outstanding-experiences.component';
 import { OutstandingCuriositiesComponent } from './dummy/outstanding-curiosities/outstanding-curiosities.component';
 import { EntertainmentComponent } from './dummy/entertainment-preview/entertainment-preview.component';
@@ -24,7 +24,7 @@ import { MatTabsModule } from '@angular/material/tabs';
   declarations: [
     HomeComponent,
     PrincipalSliderComponent,
-    OutstandingNewsComponent,
+    OutstandingGeneralArticlesComponent,
     OutstandingGaleryMediasComponent,
     OutstandingExperiencesComponent,
     OutstandingCuriositiesComponent,
