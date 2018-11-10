a6fe08c4118fc461f3ad0468a0192543ae3f80ab
NachoVazquez <nachovc1410@gmail.com>
2018-11-09 21:45:34 -0500

    Implement Impact home section

---
+++ apps/havana500-webclient/src/app/features/home/dummy/impact-portal/impact-portal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hav-impact-portal',
+  templateUrl: 'impact-portal.component.html',
+  styleUrls: ['impact-portal.component.scss']
+})
+export class ImpactPortalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
