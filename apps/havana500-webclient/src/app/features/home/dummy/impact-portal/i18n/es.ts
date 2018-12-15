9889732ef0bef5c4205901823c2d2a44d2b3e987
NachoVazquez <nachovc1410@gmail.com>
2018-12-15 17:57:49 -0500

    Add Internationalization to HomeImpact Component

---
+++ apps/havana500-webclient/src/app/features/home/dummy/impact-portal/i18n/es.ts
@@ -0,0 +1,12 @@
+import { Locale } from '@hav500workspace/shared';
+
+export const locale: Locale = {
+  lang: 'es',
+  data: {
+    IMPACT: 'IMPACTO',
+    SOCIAL: 'SOCIAL',
+    POLITICAL: 'POLÍTICO',
+    ECONOMICAL: 'ECONÓMICO',
+    HISTORICAL: 'HISTÓRICO'
+  }
+};
---
+++ apps/havana500-webclient/src/app/features/home/dummy/impact-portal/i18n/fr.ts
@@ -0,0 +1,12 @@
+import { Locale } from '@hav500workspace/shared';
+
+export const locale: Locale = {
+  lang: 'fr',
+  data: {
+    IMPACT: 'IMPACT',
+    SOCIAL: 'SOCIAL',
+    POLITICAL: 'POLITIQUE',
+    ECONOMICAL: 'ÉCONOMIQUE',
+    HISTORICAL: 'HISTORIQUE'
+  }
+};
