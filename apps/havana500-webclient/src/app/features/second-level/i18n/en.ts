4c4ab5c8353fb77ad5bb156dfb2e9a3e635ca500
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-12-21 18:22:22 -0500

    Minor fixes after the merge

--- apps/havana500-webclient/src/app/features/second-level/i18n/en.ts
+++ apps/havana500-webclient/src/app/features/second-level/i18n/en.ts
@@ -3,7 +3,7 @@ import { Locale } from '@hav500workspace/shared';
 export const locale: Locale = {
   lang: 'en',
   data: {
-    TAG_SELECTOR: 'Tags Selector'
+    TAG_SELECTOR: 'Tags Selector',
     SEE_MORE: 'See More'
   }
 };
--- apps/havana500-webclient/src/app/features/second-level/i18n/es.ts
+++ apps/havana500-webclient/src/app/features/second-level/i18n/es.ts
@@ -3,7 +3,7 @@ import { Locale } from '@hav500workspace/shared';
 export const locale: Locale = {
   lang: 'es',
   data: {
-    TAG_SELECTOR: 'Selector de Etiquetas'
+    TAG_SELECTOR: 'Selector de Etiquetas',
     SEE_MORE: 'Ver Más'
   }
 };
