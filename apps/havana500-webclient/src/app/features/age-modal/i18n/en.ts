0dae814afa9d1fd6c451b019c2e1c0796ce66663
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-16 11:20:21 -0500

    Add the translation

---
+++ apps/havana500-webclient/src/app/features/age-modal/i18n/en.ts
@@ -0,0 +1,11 @@
+import { Locale } from "@hav500workspace/shared";
+
+export const locale: Locale = {
+  lang: 'en',
+  data: {
+    buttonTextNotAdult: 'You must not be under age',
+    buttonTextAdult: 'Enter',
+    userAgePlaceholderText: 'Type your year of birth',
+    MESSAGE: ''
+  }
+};
