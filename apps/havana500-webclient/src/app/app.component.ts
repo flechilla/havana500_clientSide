2032121576365e4a9b6e219b2552156f65bac581
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-16 10:14:20 -0500

    Implement functionalities to show the modal if the cookies is not set and hide it when the user set the age

--- apps/havana500-webclient/src/app/app.component.ts
+++ apps/havana500-webclient/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { AntTranslateService } from '@hav500workspace/shared';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { CookiesService } from './core/services/cookies.service';
+import { AgeModalComponent } from './features/age-modal/age-modal.component';
 
 @Component({
   selector: 'hav-root',
@@ -8,13 +12,32 @@ import { AntTranslateService } from '@hav500workspace/shared';
 })
 export class AppComponent {
   title = 'havana500-admin';
+  private modalRef: BsModalRef;
 
-  constructor(private translateService: AntTranslateService) {
+  constructor(private translateService: AntTranslateService, 
+    private cookieService: CookiesService,
+    private modalService: BsModalService) {
     // Add languages
     this.translateService.addLanguages(['en', 'es', 'fr']);
     // Set the default language
     this.translateService.setDefaultLanguage('es');
     // Use a language
     this.translateService.useLanguage('es').subscribe();
+
+    if (!this.cookieService.isLegalAge()) {
+      this.modalRef = this.modalService.show(AgeModalComponent);
+      this.modalRef.content.onClose.subscribe(result =>{
+
+      })
+    }
+    
+  }
+
+  setUserAgeCookie(result:  number) {
+      this.cookieService.setAgeOnCookie(result)
+        .subscribe(r => {
+            this.modalRef.hide();
+        });
+      
   }
 }
