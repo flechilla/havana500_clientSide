28bc08454f74b13ab2f2e023d484f84e16aee8b2
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-16 09:27:45 -0500

    Implement methods to know if the user contains the ageCookie and to set the ageCookie

--- apps/havana500-webclient/src/app/core/services/cookies.service.ts
+++ apps/havana500-webclient/src/app/core/services/cookies.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { HavanaEnvironment } from 'libs/shared/src/lib/models';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
+import { Agent } from 'http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,16 @@ import { Observable } from 'rxjs';
 export class CookiesService {
   private setCookiesUrl: string;
   private acceptCookiesUrl: string;
+  private setAgeCookieUrl: string;
   private readonly cookiesAcceptedCookie = 'CookiesAccepted';
+  private readonly aboveAge = 'IsLegalAge'
   
     constructor(
       private environment: HavanaEnvironment,  
       private httpClient: HttpClient,
       private cookieService: CookieService) 
       {
+        this.setAgeCookieUrl = this.environment.apiUrl + "Cookies/SetAge"
         this.setCookiesUrl = this.environment.apiUrl + "Cookies/SetLanguage"
         this.acceptCookiesUrl = this.environment.apiUrl + "Cookies/AcceptCookies"
       }
@@ -33,6 +37,19 @@ export class CookiesService {
 
   hideCookieBanner() {
     return !!this.cookieService.get(this.cookiesAcceptedCookie);
+  }
+
+  isLegalAge(): boolean {
+    const ageCokie = this.cookieService.get(this.aboveAge);
+    const output = !!ageCokie;
+
+    return output;
+  }
+
+  setAgeOnCookie(userAge: number): Observable<any> {
+    return this.httpClient.post(this.setAgeCookieUrl, {
+      age: userAge
+    });
   }
 }
 
