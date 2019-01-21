67aa75f3e342ab91d80d4b0ef40f38cd3fa6b16d
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-21 15:30:58 -0500

    Implement IUser interface and extended by the User class

--- libs/shared/src/lib/models/user.model.ts
+++ libs/shared/src/lib/models/user.model.ts
@@ -1,4 +1,74 @@
-export class User {
+import { AuditableAndTrackableEntity } from ".";
+
+/**
+ *  Respresent a user in the system
+ *
+ * @export
+ * @interface User
+ * @extends {AuditableAndTrackableEntity<string>}
+ */
+export interface IUser extends AuditableAndTrackableEntity<string> {
+
+  /**
+   *  The nickname of the user
+   *
+   * @type {string}
+   * @memberof User
+   */
+  userName: string;
+
+  /**
+   * The user email
+   *
+   * @type {string}
+   * @memberof User
+   */
+  email: string;
+
+  /**
+   *  Indicates that the user has confirmed the email
+   *
+   * @type {string}
+   * @memberof User
+   */
+  emailConfirmed: string;
+
+  phoneNumber: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  /**
+   * The ref for the image of the user.
+   * User this in the `src` of the img tag
+   *
+   * @type {string}
+   * @memberof User
+   */
+  userImageHref: string;
+
+  /**
+   * The User's role
+   *
+   * @type {string}
+   * @memberof User
+   */
+  roleName: string;
+}
+
+export class User implements IUser {
+  userName: string;
+  emailConfirmed: string;
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  userImageHref: string;
+  roleName: string;
+  createdBy: string;
+  modifiedBy: string;
+  createdAt: Date;
+  modifiedAt: Date;
   constructor(
     public id: string = null,
     public email: string = null,
