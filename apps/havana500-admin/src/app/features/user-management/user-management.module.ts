29bf36a18e471618ba38947b365f5057cc296a5e
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-21 13:16:45 -0500

    Generate component to list all the users in the system

--- apps/havana500-admin/src/app/features/user-management/user-management.module.ts
+++ apps/havana500-admin/src/app/features/user-management/user-management.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AddNewUserComponent } from './add-new-user/add-new-user.component';
 import { SharedModule } from '@hav500workspace/shared';
 import { UserManagementRoutingModule } from './user-management-routing.module';
+import { ListUsersComponent } from './list-users/list-users.component';
 
 @NgModule({
-  declarations: [AddNewUserComponent],
+  declarations: [AddNewUserComponent, ListUsersComponent],
   imports: [
     SharedModule,
     UserManagementRoutingModule
