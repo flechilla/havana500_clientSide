58333d385cf1c8e51661cc5d2c1a37e1cfeca8a9
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-22 12:55:59 -0500

    Partial implementation of the user creation functionality

--- apps/havana500-admin/src/app/features/user-management/add-new-user/add-new-user.component.ts
+++ apps/havana500-admin/src/app/features/user-management/add-new-user/add-new-user.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Inject,
+  ViewEncapsulation,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  NgForm,
+  FormControl
+} from '@angular/forms';
+
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatMenuTrigger
+} from '@angular/material';
+import { Observable } from 'rxjs';
+import { antAnimations, User } from '@hav500workspace/shared';
+import { startWith, map } from 'rxjs/operators';
+import { UploadService } from '../../../core/services/http/upload.service';
 
 @Component({
   selector: 'admin-add-new-user',
@@ -6,10 +30,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new-user.component.scss']
 })
 export class AddNewUserComponent implements OnInit {
-
-  constructor() { }
+  public userForm: FormGroup;
+  public userId: string;
+  constructor(
+    protected fb: FormBuilder,
+    public dialogRef: MatDialogRef<AddNewUserComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      user$: Observable<User>;
+      userId: string;
+    },
+    private uploadService: UploadService
+  ) {}
 
   ngOnInit() {
+    this.loadForm();
   }
 
+  protected loadForm() {
+    this.userForm = this.fb.group({
+      id: '',
+      userName: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      role: '',
+      userImageHref: ''
+    });
+  }
+
+  save() {
+    console.log(this.userForm.value);
+  }
+
+  close() {
+
+  }
 }
