def4f2475fbdc42123168601d8bd2dce50585a7e
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-22 08:53:51 -0500

    Complement the userService and other improvements

--- apps/havana500-admin/src/app/core/services/user.service.ts
+++ apps/havana500-admin/src/app/core/services/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { BaseCrudService, User } from '@hav500workspace/shared';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
+import { environment } from 'libs/shared/src/lib/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
+@Injectable()
+export class UserService extends BaseCrudService<User> {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, protected snack: MatSnackBar) {
+    super(environment.apiUrl + 'account', httpClient, snack);
+   }
 }
--- apps/havana500-admin/src/app/features/user-management/user-management.module.ts
+++ apps/havana500-admin/src/app/features/user-management/user-management.module.ts
@@ -3,12 +3,16 @@ import { AddNewUserComponent } from './add-new-user/add-new-user.component';
 import { SharedModule } from '@hav500workspace/shared';
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import { ListUsersComponent } from './list-users/list-users.component';
+import { UserService } from '../../core/services/user.service';
 
 @NgModule({
   declarations: [AddNewUserComponent, ListUsersComponent],
   imports: [
     SharedModule,
     UserManagementRoutingModule
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserManagementModule { }
