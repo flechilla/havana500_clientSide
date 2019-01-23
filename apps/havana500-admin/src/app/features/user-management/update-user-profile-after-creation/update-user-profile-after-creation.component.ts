8d4e62328d8d48c3b8cb9f696ac7d57d672c2842
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-22 22:32:07 -0500

    Implement component for the email callback

---
+++ apps/havana500-admin/src/app/features/user-management/update-user-profile-after-creation/update-user-profile-after-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-update-user-profile-after-creation',
+  templateUrl: './update-user-profile-after-creation.component.html',
+  styleUrls: ['./update-user-profile-after-creation.component.scss']
+})
+export class UpdateUserProfileAfterCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- apps/havana500-admin/src/app/features/user-management/user-management.module.ts
+++ apps/havana500-admin/src/app/features/user-management/user-management.module.ts
@@ -5,12 +5,12 @@ import { UserManagementRoutingModule } from './user-management-routing.module';
 import { ListUsersComponent } from './list-users/list-users.component';
 import { UserService } from '../../core/services/user.service';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UpdateUserProfileAfterCreationComponent } from './update-user-profile-after-creation/update-user-profile-after-creation.component';
 
 @NgModule({
-  declarations: [AddNewUserComponent, ListUsersComponent, UserProfileComponent],
+  declarations: [AddNewUserComponent, ListUsersComponent, UserProfileComponent, UpdateUserProfileAfterCreationComponent],
   imports: [
-    SharedModule,
-    UserManagementRoutingModule
+    SharedModule
   ],
   providers: [
     UserService
