b5c4d8faf72403f365444a910f7d48391b5b9e54
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-23 06:02:19 -0500

    IMplement functionalities to check the user code after the callback from the email

--- apps/havana500-admin/src/app/core/services/user.service.ts
+++ apps/havana500-admin/src/app/core/services/user.service.ts
@@ -3,11 +3,29 @@ import { BaseCrudService, User } from '@hav500workspace/shared';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material';
 import { environment } from 'libs/shared/src/lib/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserService extends BaseCrudService<User> {
+  
+  private checkCodeUrl = this.url + '/confirmEmail';
 
   constructor(private httpClient: HttpClient, protected snack: MatSnackBar) {
     super(environment.apiUrl + 'account', httpClient, snack);
    }
+
+   /**
+    * Check if the user has a valid code given its
+    * Id and the code
+    *
+    * @param {string} userId
+    * @param {string} code
+    * @returns {Observable<boolean>}
+    * @memberof UserService
+    */
+   checkUserCode(userId: string, code: string): Observable<boolean> {
+    const url = this.checkCodeUrl + '?userId='
+                + userId + '&code=' + code;
+    return this.httpClient.get<boolean>(url);
+  }
 }
--- apps/havana500-admin/src/app/features/user-management/update-user-profile-after-creation/update-user-profile-after-creation.component.ts
+++ apps/havana500-admin/src/app/features/user-management/update-user-profile-after-creation/update-user-profile-after-creation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../core/services/user.service';
 
 @Component({
   selector: 'admin-update-user-profile-after-creation',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-user-profile-after-creation.component.scss']
 })
 export class UpdateUserProfileAfterCreationComponent implements OnInit {
-
-  constructor() { }
+  private userId: string;
+  private code: string;
+  private userHasValidCode: boolean;
+  constructor(private route: ActivatedRoute,
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.userId = this.route.snapshot.queryParamMap.get('userId');
+    this.code = this.route.snapshot.queryParamMap.get('code');
+    this.checkUserCode();
   }
 
+  checkUserCode(): void {
+    console.log('userid: ' + this.userId);
+    console.log('code: ' + this.code);
+    this.userService
+      .checkUserCode(this.userId, this.code)
+      .subscribe(r => {
+        this.userHasValidCode = r;
+        console.log('CheckEmailCode result: ' + r);
+      });
+
+  }
 }
