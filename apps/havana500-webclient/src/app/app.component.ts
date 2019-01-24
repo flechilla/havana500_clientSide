653af82fb5442806012cd5eb050cff5b78f85f4a
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-24 14:19:23 -0500

    Add var to show the age modal only for production
    NOTE:
    This have to be removed in production

--- apps/havana500-webclient/src/app/app.component.ts
+++ apps/havana500-webclient/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component, ChangeDetectorRef, AfterViewInit, OnInit } from '@angular/core';
+import {
+  Component,
+  ChangeDetectorRef,
+  AfterViewInit,
+  OnInit
+} from '@angular/core';
 import { AntTranslateService } from '@hav500workspace/shared';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal';
@@ -11,38 +16,43 @@ import { AgeModalComponent } from './features/age-modal/age-modal.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
- 
   title = 'havana500-admin';
   private modalRef: BsModalRef;
   private isValidAge: boolean;
-  private displayMain
+  private displayMain;
+  private isDev = true;
 
-  constructor(private translateService: AntTranslateService, 
+  constructor(
+    private translateService: AntTranslateService,
     private cookieService: CookiesService,
     private modalService: BsModalService,
-    private ref: ChangeDetectorRef) {
+    private ref: ChangeDetectorRef
+  ) {
     // Add languages
     this.translateService.addLanguages(['en', 'es', 'fr']);
     // Set the default language
     this.translateService.setDefaultLanguage('es');
     // Use a language
     this.translateService.useLanguage('es').subscribe();
-
   }
 
   ngAfterViewInit(): void {
-    setTimeout(()=>{
-    this.ageModalOn();
-    }, 1500);
+    if (!this.isDev) {
+      setTimeout(() => {
+        this.ageModalOn();
+      }, 1500);
+    }
+    else {
+      this.isValidAge = true;
+    }
   }
 
-  setUserAgeCookie(result:  number) {
-    console.log(result)
-      this.cookieService.setAgeOnCookie(result)
-        .subscribe(r => {
-            this.modalRef.hide();
-            this.isValidAge = true;
-        });      
+  setUserAgeCookie(result: number) {
+    console.log(result);
+    this.cookieService.setAgeOnCookie(result).subscribe(r => {
+      this.modalRef.hide();
+      this.isValidAge = true;
+    });
   }
 
   ageModalOn(): any {
@@ -50,12 +60,11 @@ export class AppComponent implements AfterViewInit {
       this.modalService.config.backdrop = 'static';
       this.modalService.config.keyboard = false;
       this.modalRef = this.modalService.show(AgeModalComponent);
-      this.modalRef.content.onClose.subscribe(result =>{
-          this.setUserAgeCookie(result);
+      this.modalRef.content.onClose.subscribe(result => {
+        this.setUserAgeCookie(result);
       });
       this.isValidAge = false;
-    }
-    else {
+    } else {
       this.isValidAge = true;
     }
   }
