179d5b7a4c5e2cc0ed4430558b1e14c9ea699547
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-24 06:10:21 -0500

    Cherry picked changes made by Nacho on the gallery.component

--- apps/havana500-webclient/src/app/features/gallery/gallery.component.ts
+++ apps/havana500-webclient/src/app/features/gallery/gallery.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ImagesService } from 'libs/shared/src/lib/services/http/image.service';
+import {
+  AntTranslateService,
+  MarketingImageService
+} from '@hav500workspace/shared';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ParamMap } from '@angular/router';
 
 @Component({
   selector: 'hav-gallery',
@@ -6,7 +14,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  constructor() {}
+  protected amountOfPictures: number;
 
-  ngOnInit() {}
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    private imageService: ImagesService,
+    private translateService: AntTranslateService,
+    private marketingImageService: MarketingImageService,
+    public media: MediaMatcher,
+    public changeDetectorRef: ChangeDetectorRef,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit() {
+    // Size detection
+    this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+
+    this.amountOfPictures = 16;
+
+    this.getImages();
+  }
+  isMobile(): boolean {
+    return this.mobileQuery.matches;
+  }
+
+  getImages() {
+    this.imageService.getWithPagAndSort(0, this.amountOfPictures, null, null);
+  }
 }
